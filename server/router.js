624d6e392370b4b7242239b2d118652e95c7cb0f
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-07-01 10:53:54 +0530

    Changed router

--- server/router.js
+++ server/router.js
@@ -1,10 +1,19 @@
-const createRouter = (...handlers) => (request, response) => {
-  for (const handler of handlers) {
-    if (handler(request, response)) {
-      return true;
+const createNext = (handlers) => {
+  let index = -1;
+  const callNextHandler = (request, response) => {
+    index++;
+    const currentHandler = handlers[index];
+    if (currentHandler) {
+      currentHandler(request, response, () =>
+        callNextHandler(request, response));
     }
-  }
-  return false;
+  };
+  return callNextHandler;
+};
+
+const createRouter = (...handlers) => (request, response) => {
+  const next = createNext(handlers);
+  next(request, response);
 }
 
 module.exports = { createRouter }
